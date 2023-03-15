ca897b400ef48d8d58bff1618d5cd280e57f8550
jacksuh <jacksonsdss@gmail.com>
2023-03-15 14:48:22 -0300

    HackerRank

---
+++ src/javabasico/JavaPrimalityTest.java
@@ -0,0 +1,29 @@
+package javabasico;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+
+        BigInteger e = new BigInteger(n);
+        boolean probablePrime = e.isProbablePrime(13);
+        String p = probablePrime ? "prime": "not prime";
+        System.out.println(p);
+        bufferedReader.close();
+
+    }
+}
