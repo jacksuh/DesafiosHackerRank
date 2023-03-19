d0bef5ba06843280988397ce7a5efa40a23cd24b
jacksuh <jacksonsdss@gmail.com>
2023-03-19 08:35:45 -0300

    HackerRank

---
+++ src/javabasico/JavaBigDecimal.java
@@ -0,0 +1,27 @@
+package javabasico;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class JavaBigDecimal {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+        Arrays.sort(s, 0, n, (s1, s2)-> new BigDecimal(s2).compareTo(new BigDecimal(s1)));
+
+
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+
+    }
+}
